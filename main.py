3257966132b7e006621b7f86dc0bad39a6dbc6c3
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2021-11-17 12:13:26 +0100

    added fix for strandvejen

--- main.py
+++ main.py
@@ -238,7 +238,10 @@ def get_history(user_id):
 
     print_history(history)
 
+
 is_strandvejen = False
+
+
 def sale(user, itm, count=1):
     if int(count) <= 0:
         print('Du kan ikke købe negative mængder af varer.')
@@ -300,6 +303,7 @@ Du kan foretage indbetaling via MobilePay'''
     global is_strandvejen
     if is_strandvejen:
         import time
+
         time.sleep(5)
         raise SystemExit
 
@@ -324,7 +328,9 @@ def parse(args):
     parser.add_argument('-p', '--mobilepay', dest='money', help='Provides a QR code to insert money into your account')
     parser.add_argument('-a', '--update', action='store_true', help='Update the script and then exists')
     parser.add_argument('-o', '--shorthands', action='store_true', help='Shows shorthands')
-    parser.add_argument('-x', '--strandvejen', action='store_true', help='Flag used for the CRT terminal version running in strandvejen')
+    parser.add_argument(
+        '-x', '--strandvejen', action='store_true', help='Flag used for the CRT terminal version running in strandvejen'
+    )
     parser.add_argument(
         '-s', '--setup', action='store_true', help='Creates a .sts at /home/<user> storing your account username'
     )
